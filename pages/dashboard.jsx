151a6ca4255066fa267c2db4fc843a211a2a2169
Botdeveloper358 <indonesiyaserver852101@gmail.com>
2025-07-18 01:00:46 +0530

    dashboard.jsx

---
+++ pages/dashboard.jsx
@@ -0,0 +1,10 @@
+export default function Dashboard() {
+    return (
+        <div className="min-h-screen bg-black text-white flex flex-col justify-center items-center space-y-8 p-10">
+            <h1 className="text-4xl font-bold bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 text-transparent bg-clip-text">
+                Welcome To Editing Edition 🎬
+            </h1>
+            <p className="text-gray-400">You're now inside your Dashboard. Enjoy our Premium Services!</p>
+        </div>
+    )
+}
